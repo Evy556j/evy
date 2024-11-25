7fbf9799d6824db5cec02442b41d44e325696de3
Evy556j <lohramdesouza0@gmail.com>
2024-11-25 09:29:04 -0300

    Add files via upload

---
+++ evyy/perguntas.js
@@ -0,0 +1,37 @@
+
+criaCartao(
+    'Cebola',
+    'O que é cebola?',
+    'A cebola é uma hortaliça'
+)
+
+criaCartao(
+    'Cebolas',
+    'Qual a função da cebola?',
+    'possui propriedades antivirais'
+)
+
+criaCartao(
+    'Cebola',
+    'Qual ácido tem na cebola?',
+    'Ácido sulfúrico'
+)
+
+criaCartao(
+    'Cebola',
+    'Qual a cebola mais forte?',
+    'Cebola branca'
+)
+
+criaCartao(
+    'Cebola',
+    'Qual o coletivo de cebola?',
+    'Réstia'
+    
+)
+
+criaCartao(
+    'Cebola',
+    'Qual o plural de cebola',
+    'Cebolas',
+)
